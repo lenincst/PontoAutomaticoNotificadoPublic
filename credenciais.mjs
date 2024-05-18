20c4a98e848519283b643391844f7e22f422baf7
Lenin Costa da Rosa <lenincostta@gmail.com>
2024-05-17 21:07:01 -0300

    Credenciais atualiaza

--- credenciais.mjs
+++ credenciais.mjs
@@ -1,10 +1,10 @@
 export const CREDENCIAIS = {
     GZAPPY_URL: "https://api.gzappy.com/v1/message/send-message",
-    user_token_id: 'ID',
-    instance_id: 'ID',
-    instance_token: 'ID',
+    user_token_id: '94d459c2-fee3-4bdc-b3a6-b65ed70ca56e',
+    instance_id: 'LIMGZA2N0S4ZMTTKF8ETE5SH',
+    instance_token: 'da635715-b62d-442e-83ab-686f70e061e5',
     message: ["A atividade foi concluída com sucesso!"],
-    phone: ["NUMERO"],
-    email: 'EMAIL@gmail.com',
-    senha: '@SENHA'
+    phone: ["5548988646134"],
+    email: 'profile.lenin@gmail.com',
+    senha: '@D2vveaax'
 };
